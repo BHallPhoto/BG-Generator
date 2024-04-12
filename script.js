91268e1fc4e1235f2455dc1d525eea777f772114
BHallPhoto <bristolhall@gmail.com>
2024-04-12 12:28:25 -0700

    reverted back to old way

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let color1 = document.querySelector(".color1"),
 
 function setGradient(a) {
   // console.log(a)
-  let bgColor = `linear-gradient(${a}, ${color1.value}, ${color2.value})`;
+  let bgColor = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   body.style.background = bgColor;
   css.textContent = "background: " + body.style.background + ";";
 }
@@ -20,52 +20,10 @@ color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 random.addEventListener("click", () => {
-  debugger;
-  let gradAng = "to right";
   color1.setAttribute("value", "#"+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, "0"));
   color2.setAttribute("value", "#"+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, "0"));
-  for (let i = 0; i < angles.length; i++) {
-    // console.log(angles[i].id);
-    switch (true) {
-      case angles[i].id === "to-right":
-        gradAng = "to right";
-        break;
-      case angles[i].id === "to-bottom":
-        gradAng = "to bottom";
-        break;
-      case angles[i].id === "bottom-right":
-        gradAng = "to bottom right";
-        break;
-      case angles[i].id === "bottom-left":
-        gradAng = "to bottom left";
-        break;
-      default:
-    }
-  }
-  setGradient(gradAng)
+  setGradient()
 })
-
-for (let i = 0; i < angles.length; i++) {
-  angles[i].addEventListener("click", (e) => {
-    // console.log(e.target.id)
-    switch(true) {
-      case e.target.id === "to-right":
-        setGradient("to right");
-        break;
-      case e.target.id === "to-bottom":
-        setGradient("to bottom");
-        break;
-      case e.target.id === "bottom-right":
-        setGradient("to bottom right");
-        break;
-      case e.target.id === "bottom-left":
-        setGradient("to bottom left");
-        break;
-      default:
-        setGradient("to left");      
-    }
-  })
-}
 
 button.addEventListener("click", () => {
   let copyText = document.getElementById("copytext");
